3a03b562d3038230ff5401655814c9fb2d56e86c
Alona <kkosenko.lona@gmail.com>
2021-04-18 22:14:08 +0300

    Продовження зчитування з файла

--- bmp.cpp
+++ bmp.cpp
@@ -22,7 +22,23 @@ void bmp::read_file(string file_name) {
 	in_file.read((char*)&bmp_head.width, sizeof(bmp_head.width));
 	cout << "bmp_head.width is :" << bmp_head.width << endl;
 	in_file.read((char*)&bmp_head.depth, sizeof(bmp_head.depth));
-	cout << "bmp_head.width is :" << bmp_head.width << endl;
+	cout << "bmp_head.depth is :" << bmp_head.depth << endl;
+	in_file.read((char*)&bmp_head.biPlanes, sizeof(bmp_head.biPlanes));
+	cout << "bmp_head.biPlanes is :" << bmp_head.biPlanes << endl;
+	in_file.read((char*)&bmp_head.bits, sizeof(bmp_head.bits));
+	cout << "bmp_head.bits is :" << bmp_head.bits << endl;
+	in_file.read((char*)&bmp_head.biCompression, sizeof(bmp_head.biCompression));
+	cout << "bmp_head.biCompression is :" << bmp_head.biCompression << endl;
+	in_file.read((char*)&bmp_head.biSizeImage, sizeof(bmp_head.biSizeImage));
+	cout << "bmp_head.biSizeImage is :" << bmp_head.biSizeImage << endl;
+	in_file.read((char*)&bmp_head.biXPelsPerMeter, sizeof(bmp_head.biXPelsPerMeter));
+	cout << "bmp_head.biXPelsPerMeter is :" << bmp_head.biXPelsPerMeter << endl;
+	in_file.read((char*)&bmp_head.biYPelsPerMeter, sizeof(bmp_head.biYPelsPerMeter));
+	cout << "bmp_head.biYPelsPerMeter is :" << bmp_head.biYPelsPerMeter << endl;
+	in_file.read((char*)&bmp_head.biClrUsed, sizeof(bmp_head.biClrUsed));
+	cout << "bmp_head.biClrUsed is :" << bmp_head.biClrUsed << endl;
+	in_file.read((char*)&bmp_head.biClrImportant, sizeof(bmp_head.biClrImportant));
+	cout << "bmp_head.biClrImportant is :" << bmp_head.biClrImportant << endl;
 
 
 	in_file.close();
